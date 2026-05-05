4f540b135a67fee17692fe2459c4458cedc7395c
wang86373 <130103100+wang86373@users.noreply.github.com>
2026-05-05 14:47:50 +0800

    Update create-payment.js

--- netlify/functions/create-payment.js
+++ netlify/functions/create-payment.js
@@ -49,7 +49,7 @@ exports.handler = async function (event) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        price_amount: 9.99,
+        price_amount: 12,
         price_currency: "usd",
         order_id: orderId,
         order_description: `FluentReply Pro | ${email}`,
